a4c2d59e376822314e494b48c19d21851c760d1c
Matheus Dantas <correia.dantas@outlook.com>
2021-04-07 20:38:36 -0300

    responsiveness adjusted

--- src/components/Buttons/style.js
+++ src/components/Buttons/style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Button from "@material-ui/core/Button";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Colors from "../../global/Colors";
 
 export const StyledButton = styled(Button)`
@@ -14,6 +15,10 @@ export const StyledButton = styled(Button)`
     width: 120px;
     height: 30px;
     color: ${Colors.BasicYellow};
+
+    @media (min-width: 700px) {
+      font-size: 25px;
+    }
   }
   & .MuiTouchRipple-root {
     border: 2px solid ${Colors.BasicYellow};
