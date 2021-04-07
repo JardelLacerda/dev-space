f15ee11ff2d195669f9bd5670e141d880db8fc70
Matheus Dantas <correia.dantas@outlook.com>
2021-04-07 23:04:39 +0000

    @component/buttons

---
+++ src/components/Buttons/index.jsx
@@ -0,0 +1,11 @@
+import { StyledButton } from "./style";
+
+const Button = ({ nameBtn }) => {
+  return (
+    <>
+      <StyledButton>{nameBtn}</StyledButton>
+    </>
+  );
+};
+
+export default Button;
