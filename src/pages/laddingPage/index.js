11a27da3d279d86b539c580afa7aee91ed0c5dbe
Jardel lacerda <jardellacerda2001@gmail.com>
2021-04-07 23:45:31 +0000

    @feature/ladding page

--- src/pages/laddingPage/index.js
+++ src/pages/laddingPage/index.js
@@ -1,11 +1,33 @@
 import Button from "../../components/Buttons";
+import {
+  ContainerButtons,
+  ContainerInfo,
+  ContainerMain,
+  Description,
+  SubTitle,
+  Title,
+} from "./styled";
 
 const LaddingPage = () => {
   return (
-    <>
-      <h1>LADDING PAGE</h1>
-      <Button nameBtn="Login" />
-    </>
+    <ContainerMain>
+      <ContainerInfo>
+        <Title>DevSpace</Title>
+        <SubTitle>Seu espaço de desenvolvimento</SubTitle>
+
+        <Description>
+          Mussum Ipsum, cacilds vidis litro abertis. Praesent malesuada urna
+          nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Posuere
+          libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit
+          amet nisi. Suco de cevadiss deixa as pessoas mais interessantis.
+          Sapien in monti palavris qui num significa nadis i pareci latim.
+        </Description>
+        <ContainerButtons>
+          <Button nameBtn="Register" />
+          <Button nameBtn="Login" />
+        </ContainerButtons>
+      </ContainerInfo>
+    </ContainerMain>
   );
 };
 
