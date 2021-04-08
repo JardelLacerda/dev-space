5ceb72100ba852fd02cc761fa0275966ff3a5080
Matheus Dantas <correia.dantas@outlook.com>
2021-04-08 11:24:37 -0300

    fix: update

--- src/components/Buttons/style.js
+++ src/components/Buttons/style.js
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 import Button from "@material-ui/core/Button";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Colors from "../../global/Colors";
 
 export const StyledButton = styled(Button)`
-  background-color: #6772e5;
   padding: 7px 14px;
 
   &:hover {
     background-color: #5469d4;
   }
+
+  & .MuiButton-root {
+    color: rgba(0, 0, 0, 0.5);
+  }
+
   & .MuiButton-label {
     padding: 0.5em;
     width: 120px;
