f0d739d10bb0ddb51c96d499c694a91f1d031a93
Matheus Dantas <correia.dantas@outlook.com>
2021-04-13 18:42:46 -0300

    register request done

---
+++ src/servers/register/index.js
@@ -0,0 +1,65 @@
+import Form from "../../components/Form";
+import StandardModal from "../../components/Modal";
+import Astronaut from "../../images/astronaut.png";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import axios from "axios";
+
+const RequisitionRegister = () => {
+  const schema = yup.object().shape({
+    name: yup.string().required("Campo Obrigatorio"),
+    email: yup.string().email("Email invalido").required("Campo Obrigatorio"),
+    password: yup.string().required("Campo Obrigatorio"),
+    passwordConfirm: yup
+      .string()
+      .oneOf([yup.ref("password")], "Senhas Diferentes")
+      .required("Campo obrigatório!"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    axios
+      .post("https://dev-space-json-server.herokuapp.com/register", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(() => {
+        reset();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <StandardModal buttonTxtOpen="Cadastro">
+      <Form
+        instructions={{
+          buttonName: "Cadastrar",
+          icone: { icone: Astronaut, width: "150px" },
+          inputList: [
+            ["name", "digite seu nome"],
+            ["email", "digite seu email"],
+            ["password", "digite sua senha"],
+            ["passwordConfirm", "confirme sua senha"],
+          ],
+          formAction: handleSubmit(onSubmit),
+          register: register,
+          errors: errors,
+        }}
+      />
+    </StandardModal>
+  );
+};
+
+export default RequisitionRegister;
