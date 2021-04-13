48e31a09667ae0de99048babcec97c18dd964bcf
Matheus Dantas <correia.dantas@outlook.com>
2021-04-13 17:34:29 -0300

    improved border os Modal

--- src/components/Modal/style.js
+++ src/components/Modal/style.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const Paper = styled.div`
-  background-color: gray;
   box-shadow: gray;
-  margin: 0;
+  width: auto;
   display: flex;
   flex-flow: column wrap;
   align-items: center;
+
+  :focus {
+    outline: none;
+  }
 `;
