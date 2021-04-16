5b26ecf2e5de157a376247cfbffc52ca8230dd3b
Matheus Henrique Costa <matheus2hcosta@gmail.com>
2021-04-16 12:16:11 +0000

    Feature/user patch

--- src/pages/profile/style.js
+++ src/pages/profile/style.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 export const Container = styled.div`
-  width: 100vw;
   min-height: 100vh;
   background-color: ${({ theme }) => theme.PrimaryTheme};
   display: flex;
@@ -20,29 +19,32 @@ export const Container = styled.div`
 `;
 
 export const Info = styled.div`
-  width: 30vw;
-  height: 60vh;
+  flex-basis: 370px;
+  height: 400px;
 `;
 export const UserInfo = styled.div`
   background-color: ${({ theme }) => theme.SecundaryTheme};
   border-radius: 10px;
-  height: 95%;
+  height: 90%;
   display: flex;
   flex-flow: column wrap;
   justify-content: space-around;
 `;
 export const EditInfo = styled.div`
-  width: 30vw;
-  height: 60vh;
+  flex-basis: 370px;
+  height: 400px;
   display: flex;
   flex-flow: column wrap;
   justify-content: space-around;
+  button {
+    display: inline-block;
+  }
 `;
 
 export const ProfileImage = styled.div`
   background-color: ${({ theme }) => theme.SecundaryTheme};
   border-radius: 10px;
-  height: 10vh;
+  height: 20%;
   display: flex;
   flex-flow: row-wrap;
   align-items: center;
@@ -58,23 +60,30 @@ export const ProfileImage = styled.div`
 export const PersonalInfo = styled.div`
   background-color: ${({ theme }) => theme.SecundaryTheme};
   border-radius: 10px;
-
-  height: 40vh;
+  height: 58%;
   display: flex;
   flex-flow: column wrap;
   justify-content: space-around;
-`;
-export const PersonalInfoEdit = styled.div`
-  display: flex;
-  flex-flow: row wrap;
+  input {
+    border: none;
+    font-family: "Roboto Mono", monospace;
+  }
 `;
 
+export const Config = styled.div`
+  color: ${({ theme }) => theme.Details};
+  font-weight: bolder;
+  height: 10%;
+`;
+export const Div = styled.div`
+  padding: 2px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+`;
 export const ApiText = styled.span`
   background-color: ${({ theme }) => theme.SecundaryTheme};
   font-weight: 200 !important;
   border-radius: 2px;
-`;
-export const Config = styled.div`
-  color: ${({ theme }) => theme.Details};
-  font-weight: bolder;
 `;
