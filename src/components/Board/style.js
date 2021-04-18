841a1c012e050144927eee3e951db69dd7ebed43
Matheus Dantas <correia.dantas@outlook.com>
2021-04-18 12:49:38 -0300

    fix: scroll bar in Board added

--- src/components/Board/style.js
+++ src/components/Board/style.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  width: 100%;
   margin-top: 8px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  width: 98%;
+  height: 210px;
+  margin: 0 auto;
+  display: flex;
 `;
